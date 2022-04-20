804468bb2dccfff78b393b883730365dd9ca2d83
John Caldwell <jp.caldwell30@gmail.com>
2022-04-20 17:37:05 -0400

    Update lovelace-multiline-text-input-card-attributes.js

--- lovelace-multiline-text-input-card-attributes.js
+++ lovelace-multiline-text-input-card-attributes.js
@@ -1,9 +1,7 @@
 ((LitElement) => {
 	const html = LitElement.prototype.html;
 	const css = LitElement.prototype.css;
-
 	class LovelaceMultilineTextInput extends LitElement {
-
 		static get properties() {
 			return {
 				_hass: {},
@@ -12,7 +10,6 @@
 				state: {}
 			}
 		}
-
 		static get styles() {
 			return css`
 				.flex {
@@ -95,7 +92,6 @@
 				}
 			`;
 		}
-
 		updated() {
 			let _this = this;
 			this.updateComplete.then(() => {
@@ -106,7 +102,6 @@
 				}
 			});
 		}
-
 		render() {
 			return this.stateObj ? html`
 				<ha-card .hass="${this._hass}" .config="${this._config}" class="background">
@@ -124,31 +119,25 @@
 				  		</div>` : null}
 				</ha-card>` : null;
 		}
-
 		getCardSize() {
 			return Math.round(this.scrollHeight / 50);
 		}
-
 		renderButton(key) {
 			return this.state.buttons[key]
 				? html`<div class="button" title="${this.state.hints[key]}" id="button-${key}" @tap="${() => { if(this.callAction(key) !== false) { this.callService(key); }}}"><ha-icon icon="${this.state.icons[key]}"></ha-icon></div>`
 				: null;
 		}
-
 		getState() {
 			const value = this.stateObj ? this.stateObj.state : this.state.default_text;
 			return value.replace("\\n", "\n");
 		}
-
 		getText() {
 			return this.shadowRoot ? this.shadowRoot.querySelector(".textarea").value : "";
 		}
-
 		setText(val, entity_update) {
 			if(!this.shadowRoot) {
 				return false;
 			}
-
 			if(entity_update === true) {
 				this.state.last_updated_text = val;
 			}
@@ -157,12 +146,10 @@
 			this.resizeTextarea();
 			this.updateCharactersInfoText();
 		}
-
 		clearText() {
 			clearTimeout(this.state.autosave_timeout);
 			this.setText('');
 		}
-
 		pasteText() {
 			clearTimeout(this.state.autosave_timeout);
 			if(!this.shadowRoot) {
@@ -181,7 +168,6 @@
 				}
 			}
 		}
-
 		onKeyupTextarea() {
 			if(this.state.autosave) {
 				clearTimeout(this.state.autosave_timeout);
@@ -195,7 +181,6 @@
 			this.updateCharactersInfoText();
 			this.resizeTextarea();
 		}
-
 		updateCharactersInfoText() {
 			if(!this.shadowRoot) {
 				return false;
@@ -203,12 +188,10 @@
 			let textLength = this.shadowRoot.querySelector(".textarea").value.length;
 			let button_save = this.shadowRoot.querySelector("#button-save");
 			let disable_button = false;
-
 			if(this.state.max_length !== false) {
 				let maxCharactersInfoText = `${textLength}/${this.state.max_length} max.`;
 				let maxCharactersElem = this.shadowRoot.querySelector("#spanMaxCharactersInfoText")
 				maxCharactersElem.innerHTML = maxCharactersInfoText;
-
 				if(textLength >= this.state.max_length) {
 					maxCharactersElem.classList.add("text-red");
 					disable_button = true;
@@ -220,12 +203,10 @@
 					disable_button = false;
 				}
 			}
-
 			if(this.state.min_length > 0) {
 				let minCharactersInfoText = `${textLength}/${this.state.min_length} min.`;
 				let minCharactersElem = this.shadowRoot.querySelector("#spanMinCharactersInfoText")
 				minCharactersElem.innerHTML = minCharactersInfoText;
-
 				if(textLength < this.state.min_length) {
 					minCharactersElem.classList.remove("hidden");
 					disable_button = true;
@@ -234,7 +215,6 @@
 					minCharactersElem.classList.add("hidden");
 				}
 			}
-
 			if(button_save) {
 				if(disable_button) {
 					button_save.classList.add("button-disabled");
@@ -246,7 +226,6 @@
 				}
 			}
 		}
-
 		resizeTextarea() {
 			if(!this.shadowRoot) {
 				return false;
@@ -258,13 +237,11 @@
 			textArea.style.height = (parseFloat(textArea.scrollHeight) + parseFloat(textAreaComputedStyle.paddingBottom) + parseFloat(textAreaComputedStyle.borderBottomWidth)) + "px";
 			textArea.style.overflowY = "hidden";
 		}
-
 		callAction(action) {
 			if (typeof this.state.actions[action] === 'function') {
 				return this.state.actions[action]();
 			}
 		}
-
 		callService(service) {
 			if(this.state.entity_type === 'input_text' || this.state.entity_type === 'var') {
 				let value = (typeof this.state.service_values[service] === 'function' ? this.state.service_values[service]() : this.state.service_values[service]);
@@ -272,6 +249,7 @@
 					let _this = this;
 					const serviceData = {
                         			entity_id: 'var.presence_authed',
+                        			value: 'on',
                         			state: 'on',
                         			auth_url: value	  
                     			};
@@ -279,18 +257,14 @@
 				}
 			}
 		}
-
 		displayMessage(service, success) {
 			if(!this.state.show_success_messages || !this.shadowRoot || !service || service.length < 1) {
 				return;
 			}
-
 			let serviceMessageContainer = this.shadowRoot.querySelector('#serviceMessage');
-
 			if(!serviceMessageContainer) {
 				return;
 			}
-
 			// todo: translations
 			let message = "";
 			if(success) {
@@ -303,14 +277,12 @@
 					message = "An error occurred in the backend while saving!";
 				}
 			}
-
 			if(message.length > 0) {
 				serviceMessageContainer.innerHTML = message;
 				serviceMessageContainer.classList.remove("invisible");
 				serviceMessageContainer.classList.remove("opacity-0");
 				let buttons = this.shadowRoot.querySelectorAll(".button");
 				buttons.forEach(elem => elem.classList.add("opacity-0"));
-
 				setTimeout(function() {
 					serviceMessageContainer.classList.add("opacity-0");
 					buttons.forEach(elem => elem.classList.remove("opacity-0"));
@@ -321,36 +293,30 @@
 				}, 2000);
 			}
 		}
-
 		actionSave() {
 			let len = this.getText().length;
 			let length_check = len >= this.state.min_length && (this.state.max_length === false || len <= this.state.max_length);
 			return length_check;
 		}
-
 		actionClear() {
 			this.clearText();
 			if(this.state.save_on_clear && this.callAction('save') !== false) {
 				this.callService('save');
 			}
 		}
-
 		actionPaste() {
 			this.pasteText();
 		}
-
 		setConfig(config) {
 			const supported_entity_types = [
 				'input_text',
 				'var', 		// custom component: https://community.home-assistant.io/t/custom-component-generic-variable-entities/128627
 			];
-
 			const actions = {
 				save: () => { return this.actionSave(); },
 				paste: () => { return this.actionPaste(); },
 				clear: () => { return this.actionClear(); },
 			};
-
 			// paste has no service, clear must be "confirmed" by saving
 			const services = {
 				'input_text': {
@@ -360,36 +326,30 @@
 					save: 'set',
 				},
 			};
-
 			const service_values = {
 				save: () => { return this.getText(); },
 				paste: null,
 				clear: '',
 			};
-
 			const buttons = {
 				save: 1,
 				paste: 2,
 				clear: 3,
 			};
-
 			const icons = {
 				save: 'mdi:content-save-outline',
 				paste: 'mdi:content-paste',
 				clear: 'mdi:trash-can-outline',
 			};
-
 			const hints = {
 				save: 'Save',
 				paste: 'Paste from clipboard',
 				clear: 'Clear text'
 			};
-
 			let entity_type = config.entity.split('.')[0];
 			if (!config.entity || !supported_entity_types.includes(entity_type)) {
 				throw new Error('Please define an entity of type: ' + supported_entity_types.join(', '));
 			}
-
 			this.state = {
 				autosave: config.autosave === true,
 				entity: config.entity,
@@ -399,13 +359,11 @@
 				save_on_clear: config.save_on_clear === true,
 				show_success_messages: config.show_success_messages !== false,
 				title: config.title,
-
 				autosave_timeout: null,
 				default_text: "",
 				entity_type: entity_type,
 				last_updated_text: null,
 				showButtons: config.buttons !== false,
-
 				actions: Object.assign({}, actions),
 				buttons: Object.assign({}, buttons, config.buttons),
 				buttons_ordered: {},
@@ -414,7 +372,6 @@
 				service: Object.assign({}, services[entity_type]),
 				service_values: Object.assign({}, service_values),
 			};
-
 			// filter out invalid values and buttons not to be displayed
 			let state_buttons = Object.fromEntries(Object.entries(this.state.buttons).filter(([key, value]) => value === true || (!isNaN(value) && value !== 0)));
 			// get ordered button keys
@@ -422,22 +379,17 @@
 			// rebuild object with key => value
 			this.state.buttons_ordered = {};
 			state_buttons.forEach(key => this.state.buttons_ordered[key] = this.state.buttons[key]);
-
 			this.state.min_length = Math.max(this.state.min_length, 0);
-
 			if(this.state.max_length !== false && this.state.max_length <= 0) {
 				throw new Error("The max length should be greater than zero.");
 			}
 			if(this.state.min_length > this.state.max_length && this.state.max_length !== false) {
 				throw new Error("The min length must not be greater than max length.");
 			}
-
 			this._config = config;
 		}
-
 		set hass(hass) {
 			this._hass = hass;
-
 			if(hass && this._config) {
 				this.stateObj = this._config.entity in hass.states ? hass.states[this._config.entity] : null;
 				if(this.stateObj) {
@@ -448,7 +400,6 @@
 						if(this.stateObj.attributes.mode !== "text") {
 							throw new Error("An input_text entity must be in 'text' mode!");
 						}
-
 						if(this.state.min_length === 0 && (this.stateObj.attributes.min || 0) > 0) {
 							this.state.min_length = this.stateObj.attributes.min;
 							console.warn("Entity " + this._config.entity + " requires at least " + this.stateObj.attributes.min + " characters.");
@@ -464,6 +415,5 @@
 			}
 		}
 	}
-
 	customElements.define('lovelace-multiline-text-input-card-attributes', LovelaceMultilineTextInput);
 })(window.LitElement || Object.getPrototypeOf(customElements.get("hui-masonry-view") || customElements.get("hui-view")));
